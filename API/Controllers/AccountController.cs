ad87faa130af3a03110fdede151ae35ae5d6e69a
Dirk Robinson <tiaan.robinson@gmail.com>
2022-05-14 12:23:23 +0200

    Add Account Controller with a register endpoint

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using API.Entities;
+using System.Security.Cryptography;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context) //constructor
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        {
+            using var hmac = new HMACSHA512(); //'using' to ensure dispose
+
+            var user = new AppUser
+            { //Properties
+                UserName = username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user); //database.table.method(object)
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
