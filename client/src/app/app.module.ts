2df27bff36979c046cd5aa0165e5507629fa1de6
Dirk Robinson <tiaan.robinson@gmail.com>
2022-05-17 15:26:02 +0200

    Added Components (lists, messages & member-lists)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,13 +10,19 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
