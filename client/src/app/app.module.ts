60f8e8b483e57545368df858402970183bb7ac18
Dirk Robinson <tiaan.robinson@gmail.com>
2022-05-23 16:22:38 +0200

    Added not found page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { ErrorInterceptor } from './_interceptors/error.interceptor';
     MemberListComponent,
     ListsComponent,
     MessagesComponent,
-    TestErrorsComponent
+    TestErrorsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
