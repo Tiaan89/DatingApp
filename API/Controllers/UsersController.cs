3af46fb7d4ef25762f0631e4beab23214b2a2373
Dirk Robinson <tiaan.robinson@gmail.com>
2022-06-01 19:54:59 +0200

    Added AutoMapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -11,6 +11,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
 using API.Interfaces;
+using API.DTOs;
+using AutoMapper;
 
 namespace API.Controllers
 {
@@ -20,24 +22,29 @@ namespace API.Controllers
     public class UsersController : BaseApiController
     {
         private readonly IUserRepository _userRepository; //Field
+        private readonly IMapper _mapper;
 
        private readonly DataContext _context;
-       public UsersController(IUserRepository userRepository)
+       public UsersController(IUserRepository userRepository, IMapper mapper)
        {
+            _mapper = mapper;
             _userRepository = userRepository;
        }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() //IEnumerable almost like a list
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers() //IEnumerable almost like a list
         {
-            return Ok(await _userRepository.GetUsersAsync()); //Wrap in Ok response
+            var users = await _userRepository.GetUsersAsync(); 
+            var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+            return Ok(usersToReturn); //Wrap in Ok response
         }
 
         //api/users/3
         [HttpGet("{username}")] //will give you a 204 No Content Error!
-        public async Task<ActionResult<AppUser>> GetUser(string username)
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            return await _userRepository.GetUserByUsernameAsync(username);
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+            return _mapper.Map<MemberDto>(user);
         }
         
     }
